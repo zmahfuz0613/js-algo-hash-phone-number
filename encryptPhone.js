41a876430b40e2414b26f3ad6a370be7981cfe87
Andre Pato <anpato1994@gmail.com>
2020-02-04 21:37:39 -0500

    initialize repo

---
+++ encryptPhone.js
@@ -0,0 +1,9 @@
+/**
+ * @function encryptPhoneNumber should take a phone number and hash it for security and privacy purposes.
+ * @param number will always be a string formatted as such '111-223-1123'. Your function should return the first 6 digits as * and only the last 4 shown. Ex:
+ * ' 111-223-1123 ' should be ' ***-***-1123 '
+ */
+
+const encryptPhoneNumber = number => {}
+
+module.exports = encryptPhoneNumber
